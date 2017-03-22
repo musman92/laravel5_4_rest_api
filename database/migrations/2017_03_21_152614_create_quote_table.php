20ee068354c3a515d7a83467870c93355958bb7e
Usman <musmannadeem92@gmail.com>
2017-03-22 18:15:22 +0500

    first commit

---
+++ database/migrations/2017_03_21_152614_create_quote_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateQuoteTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(){
+         Schema::create('quotes', function (Blueprint $table) {
+            $table->increments('id');
+            $table->text('content');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down(){
+        Schema::dropIfExists('quotes');
+    }
+}
